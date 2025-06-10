0b0ec1a954a90f5468e91c0eaaccc5e010941056
MohammadDwikat1 <mohammaddwikat0059@gmail.com>
2025-06-10 12:49:25 +0300

    frontend and backend

---
+++ web2project/example-app/app/Models/Doctors.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class Doctors extends Model
+{
+    protected $primaryKey = 'doctor_id';
+    public $timestamps = false;
+    use HasFactory;
+protected $table = 'doctors';
+ 
+protected $fillable = ['users_id','medical_field', 'phone'];
+
+   public function user()
+{
+    return $this->belongsTo(Users_accounts::class, 'users_id', 'user_id');
+    
+}
+
+
+
+
+}
