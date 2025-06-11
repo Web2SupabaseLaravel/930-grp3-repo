7c53925881c59a79e038a2af35d57aeb768f7209
zeyad <s12218555@stu.najah.edu>
2025-06-11 23:48:18 +0300

    New Edit

--- laravel1pro/example-app/routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\UserAccountController;
-
-Route::apiResource('users', UserAccountController::class);
