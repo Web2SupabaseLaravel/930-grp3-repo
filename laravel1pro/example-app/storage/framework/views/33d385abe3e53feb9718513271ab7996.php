2168eb818cc763a578d185c5b1b7cc59f01dfe6f
zeyad <s12218555@stu.najah.edu>
2025-06-12 01:49:48 +0300

    New main

--- laravel1pro/example-app/storage/framework/views/33d385abe3e53feb9718513271ab7996.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Not Found')); ?>
-<?php $__env->startSection('code', '404'); ?>
-<?php $__env->startSection('message', __('Not Found')); ?>
-
-<?php echo $__env->make('errors::minimal', array_diff_key(get_defined_vars(), ['__data' => 1, '__path' => 1]))->render(); ?><?php /**PATH C:\Users\hp\Desktop\930-grp3-repo\laravel1pro\example-app\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AppointmentApiController;
 use App\Http\Controllers\ServiceController;
-
-Route::prefix('appointments')->group(function () {
-    Route::get('/', [AppointmentApiController::class, 'index']);
-    Route::post('/', [AppointmentApiController::class, 'store']);
-    Route::get('/{id}', [AppointmentApiController::class, 'show']);
-    Route::put('/{id}', [AppointmentApiController::class, 'update']);
-    Route::delete('/{id}', [AppointmentApiController::class, 'destroy']);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::apiResource('services', ServiceController::class);
