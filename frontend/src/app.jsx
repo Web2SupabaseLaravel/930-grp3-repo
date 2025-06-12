8d155d09c9dfd5a474cfe84f80fbb14460febaa4
zeyad <s12218555@stu.najah.edu>
2025-06-12 23:03:51 +0300

    Resolve conflict while pulling from main

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,17 +1,21 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ServicesList from './components/ServicesList';
 import ServiceForm from './components/ServiceForm';
-
+import BookAppointment from './components/BookAppointment';
 
 function App() {
   return (
     <Routes>
-      
+      {/* إعادة توجيه الصفحة الرئيسية */}
       <Route path="/" element={<Navigate to="/admin/services" />} />
 
+      {/* واجهة الإدارة */}
       <Route path="/admin/services" element={<ServicesList />} />
       <Route path="/admin/services/create" element={<ServiceForm />} />
       <Route path="/admin/services/edit/:id" element={<ServiceForm isEdit />} />
+
+      {/* صفحة حجز المواعيد للمستخدمين */}
+      <Route path="/book" element={<BookAppointment />} />
     </Routes>
   );
 }
